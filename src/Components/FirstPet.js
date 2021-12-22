38f608b692d993d46044cbaf9992222bcc790ca5
TheodoreFrazier <theo.frazier@icloud.com>
2021-12-21 22:19:49 -0500

    FirstPet.js seems to be working

--- src/Components/FirstPet.js
+++ src/Components/FirstPet.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, {useState} from "react"; //import the useState
 import "./FirstPet.css";
 
-class FirstPet extends React.Component {
-  render() {
-    return (
-      <section className="first-pet">
-        <h4>Is this your first pet?</h4>
-        <br />
-        <form>
-          <input type="checkbox" />
-        </form>
-        <h5></h5>
-      </section>
-    );
+
+
+
+
+const FirstPet = () => {
+
+  const [firstPet, setFirstPet] = useState(false); //set the state to false as we are asking if this is the first pet. Initial state of both the firstPet & checkPet event.
+
+
+  const checkPet = (event) => {
+    setFirstPet(event.target.checked) //setting the checkmark event 
   }
-}
+
+  return (
+    <section className="first-pet">
+      <h4>Is this your first pet?</h4>
+      <br />
+      <form>
+        <input type="checkbox" onClick={checkPet} />
+      </form>
+      <h5>{firstPet ? "Yes" : "No"}</h5>
+    </section>
+  );
+
+};
 
 export default FirstPet;
